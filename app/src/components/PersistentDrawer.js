a389a7537d35e5f20ae4212833b511b137ab406a
cpadua912 <cristian.padua912@gmail.com>
2021-06-30 18:19:04 -0400

    Added overlay drawer menu for navbar

---
+++ app/src/components/PersistentDrawer.js
@@ -0,0 +1,155 @@
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+
+const drawerWidth = "100%";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    background: "none",
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeIn,
+      duration: theme.transitions.duration.complex,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    background: "rgba(0, 0, 0, 0.9)",
+    color: "white",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "center",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  listItemTextCenter: {
+    width: "100%",
+    textAlign: "center",
+    fontSize: 45,
+  },
+}));
+
+export default function PersistentDrawer() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="center"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose} color="inherit">
+            {theme.direction === "ltr" ? (
+              <CloseIcon fontSize="large" />
+            ) : (
+              <CloseIcon />
+            )}
+          </IconButton>
+        </div>
+        <List>
+          {["Links", "Projects", "Resume"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemText
+                primary={text}
+                classes={{ primary: classes.listItemTextCenter }}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+      </main>
+    </div>
+  );
+}
--- app/src/components/header.js
+++ app/src/components/header.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { AppBar, Collapse, IconButton, Toolbar } from "@material-ui/core";
+import { Collapse, IconButton } from "@material-ui/core";
 import { Link as Scroll } from "react-scroll";
+import PersistentDrawer from "./PersistentDrawer";
 
 import MenuSharpIcon from "@material-ui/icons/MenuSharp";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
@@ -11,27 +12,11 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    height: "100vh",
+    height: "80vh",
   },
-  appbar: {
-    background: "none",
-  },
-  appbarWrapper: {
-    width: "80%",
-    margin: "0 auto",
-  },
-  appbarTitle: {
-    flexGrow: "1",
-    fontFamily: "Josefin Sans, sans-serif",
-    fontSize: "24px",
-  },
+
   container: {
     textAlign: "center",
-  },
-  icon: {
-    color: "white",
-    fontSize: "35px",
-    margin: "5px 15px",
   },
   landingDesc: {
     flexGrow: "1",
@@ -56,29 +41,25 @@ export default function Header() {
     setChecked(true);
   });
   return (
-    <div className={classes.root} id="header">
-      {/*<AppBar className={classes.appbar} elevation={0}>
-        <Toolbar className={classes.appbarWrapper}>
-          <IconButton>
-            <MenuSharpIcon className={classes.icon} />
-          </IconButton>
-        </Toolbar>
-      </AppBar> */}
-      <Collapse
-        in={checked}
-        {...(checked ? { timeout: 1000 } : {})}
-        collapsedHeight={50}
-      >
-        <div className={classes.container}>
-          <h1 className={classes.landingTitle}>CRISTIAN PADUA</h1>
-          <h2 className={classes.landingDesc}>Blockchain Developer</h2>
-          <Scroll to="body" smooth={true}>
-            <IconButton>
-              <ExpandMoreIcon className={classes.scrollDown} />
-            </IconButton>
-          </Scroll>
-        </div>
-      </Collapse>
-    </div>
+    <>
+      <PersistentDrawer />
+      <div className={classes.root} id="header">
+        <Collapse
+          in={checked}
+          {...(checked ? { timeout: 1000 } : {})}
+          collapsedHeight={50}
+        >
+          <div className={classes.container}>
+            <h1 className={classes.landingTitle}>CRISTIAN PADUA</h1>
+            <h2 className={classes.landingDesc}>Blockchain Developer</h2>
+            <Scroll to="body" smooth={true}>
+              <IconButton>
+                <ExpandMoreIcon className={classes.scrollDown} />
+              </IconButton>
+            </Scroll>
+          </div>
+        </Collapse>
+      </div>
+    </>
   );
 }
